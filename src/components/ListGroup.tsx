81ac1025c9f1b82fef3d66bb7355f66b7637f353
Vy Tran <vytran1805@gmail.com>
2023-04-27 15:46:37 -0400

    Finished 50:45 - Managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,41 @@
+import { MouseEvent, useState } from "react";
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //   Convert each item to <li> element, use curly bracket to render data dynamically
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //let selectedIndex = 0; // 0 means the first item is selected
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // Event handler: Type of event in React is MouseEvent => import MouseEvent from react
+  const handleClick = (event: MouseEvent) => console.log(event);
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // Convert each item to <li> element, use curly bracket to render data dynamically
   return (
     //the line below is translated to React.createElement('h1');
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li> //each item of a list should have a unique key
-        ))}
+        {items.map(
+          (
+            item, //first parameter is the item of the list
+            index // second is the index of the item
+          ) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }} //we don't call the function, we just pass the reference
+            >
+              {item}
+            </li> //each item of a list should have a unique key
+            // or the console browser will show error
+          )
+        )}
       </ul>
     </>
   );
