b0b36ca64c645070753b58e6da11a4cc87a5b808
VyT <vytran1805@gmail.com>
2023-08-04 00:07:15 -0400

    add select option, keep track of selected category using useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import ProductList from "./components/ProductList";
 function App() {
+  // keep track of the selected category so when the state is changed, our ProductList is re-rendered
+  const [selectedCategory, setSelectedCategory] = useState("");
   //use Ref hook to get reference to the input field
   const ref = useRef<HTMLInputElement>(null);
 
@@ -17,7 +19,17 @@ function App() {
   return (
     <div>
       <input ref={ref} type="text" className="form-control" />
-      <ProductList />
+      <select
+        name=""
+        id=""
+        className="form-select"
+        onChange={(event) => setSelectedCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={selectedCategory} />
     </div>
   );
 }
