4f767c1e30b99e574d45f604fd10d2d15bd2d3d6
VyT <vytran1805@gmail.com>
2023-08-04 00:08:27 -0400

    define a shape of props, add category as a dependency in effect hook

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-const ProductList = () => {
+interface Props {
+  category: string;
+}
+
+const ProductList = ({ category }: Props) => {
   // declare a state variable for storing ourlist of products
   const [products, setProducts] = useState<string[]>([]);
   // use the effect hook to call the server to fetch the products
   //   This will be executed after each render
   useEffect(() => {
-    console.log("fetching products");
+    console.log("fetching products in " + category);
     setProducts(["Clothing", "Household"]); //set products to a new array
-  },[]);
+  }, [category]);
   return <div>ProductList</div>;
 };
 
