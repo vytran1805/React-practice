a46ae7a14b33d0a0d2406c5e02de6f0e7f5688a6
VyT <vytran1805@gmail.com>
2023-08-04 12:44:54 -0400

    Finish Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import ProductList from "./components/ProductList";
-const connect = () => console.log('connecting');
-const disconnect = () => console.log('disconnecting');
- 
-function App() {
- useEffect(()=>{
-  // connecting...
-  connect();
-  // then return the cleanup function that will execute the disconnect()
-  return () => disconnect();  //this it will stop the connection
- })
-  return (
-    <div>
 
-    </div>
+import axios from "axios";
+import { map } from "zod";
+// define the shape of our user to avoid accessing invalid properties
+interface User {
+  id: number;
+  name: string;
+}
+function App() {
+  // declare a state variable for storing our users, initialize this to an empty array
+  const [users, setUsers] = useState<User[]>([]);
+  // use Effect hook to call the server
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
