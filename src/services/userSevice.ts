50771d68bb5444752d325dff0d0684290145852f
VyT <vytran1805@gmail.com>
2023-08-09 00:11:31 -0400

    Finish Extracting  User service

--- src/services/userSevice.ts
+++ src/services/userSevice.ts
@@ -19,5 +19,20 @@ class UserService {
     // treat this cancel method as a cancel button, the users use it without knowing the logic behind the button
     return { request, cancel: () => controller.abort() };
   }
+
+  deleteUser(id:number) {
+    const request = apiClient.delete("/users/" + id);
+    return { request };
+  }
+
+  addUser(user: User) {
+    const request = apiClient.post("/users", user);
+    return { request };
+  }
+
+  updateUser(updatedUser:User){
+    const request = apiClient.put("/users/" + updatedUser.id, updatedUser);
+    return { request };
+  }
 }
 export default new UserService();
